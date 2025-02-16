b28aca880fe6ac3cf25ee25768657f130be1e42d
Quantitative Imaging Research and Artificial Intelligence Lab. <105630848+qirail@users.noreply.github.com>
2025-02-16 20:54:08 +0000

    Collaborators: Grid Layout

--- src/components/collab.js
+++ src/components/collab.js
@@ -1,29 +1,24 @@
-import React from 'react'
-import CollabCard from "./collabCard"
-import { collabdata } from "../data/collaborators.data"
+import React from 'react';
+import CollabCard from "./collabCard";
+import { collabdata } from "../data/collaborators.data";
 
 const Collab = () => {
   return (
-
-
     <section id='collab' className="py-6 dark:bg-gray-800 dark:text-gray-100">
       <div className="container flex flex-col items-center justify-center p-4 mx-auto sm:p-10">
-        <h1 className="text-4xl font-bold leading-none text-center sm:text-5xl mb-5">Collaborating Groups</h1>
-        <div className="flex flex-row flex-wrap justify-center items-center mt-8 space-y-3">
-          {
-            collabdata.map((item, index) => (
-              <CollabCard key={index}
-                props={item} />
-
-            ))
-          }
+        <h1 className="text-4xl font-bold leading-none text-center sm:text-5xl mb-5">
+          Collaborating Groups
+        </h1>
+        <div
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-8"
+        >
+          {collabdata.map((item, index) => (
+            <CollabCard key={index} props={item} />
+          ))}
         </div>
       </div>
-    </section >
+    </section>
+  );
+};
 
-  )
-}
-
-export default Collab
-
-
+export default Collab;
